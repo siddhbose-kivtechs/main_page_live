8c4c1e6b1aba2fa42881b5adbf63a593e19baf25
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-07-06 21:46:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,26 +6,24 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-// app.set('view engine', 'ejs'); // Set EJS as the template engine
+
+app.set('view engine', 'ejs'); // Set EJS as the template engine
 app.set('views', path.join(__dirname, 'views'));
 
 // Enable CORS for specific origin
 app.use(cors());
 
-// Parse request body and extended the size to 1mb
+// Parse request body and extend the size to 1mb
 app.use(bodyParser.json({ limit: '1mb' }));
 app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
 
 // GET route
 app.get("/", (req, res) => {
-  // const latitude = process.env['forwarded']['x-vercel-ip-latitude']; // Get the latitude value from environment variable
-  // const longitude = process.env['forwarded']['x-vercel-ip-longitude']; // Get the longitude value from environment variable
-  let latitude=25;
-  let longitude=35;
+  let latitude = 25;
+  let longitude = 35;
 
   // Render the client-side JavaScript file dynamically with the values embedded
-  res.render('client.ejs', { latitude, longitude });
-  
+  res.render('client', { latitude, longitude });
 });
 
 // POST route

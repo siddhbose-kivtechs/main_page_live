02bef62d9498459a601e50abebfe4fef9b0b5dfe
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-07-06 21:27:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,22 +6,17 @@ const ejs = require('ejs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs'); // Set EJS as the template engine
+
 // Enable CORS for specific origin
-app.use(cors({
-//   origin: "https://example.com"
-}));
+app.use(cors());
 
 // Parse request body and extended the size to 1mb
-
 app.use(bodyParser.json({ limit: '1mb' }));
 app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
 
 // GET route
 app.get("/", (req, res) => {
-  let data = {};
-  data["GET"] = req.query;
-  res.send(data);
-    const latitude = process.env['forwdaded']['x-vercel-ip-latitude']; // Get the latitude value from environment variable
+  const latitude = process.env['forwdaded']['x-vercel-ip-latitude']; // Get the latitude value from environment variable
   const longitude = process.env['forwdaded']['x-vercel-ip-longitude']; // Get the longitude value from environment variable
 
   // Render the client-side JavaScript file dynamically with the values embedded
@@ -31,8 +26,8 @@ app.get("/", (req, res) => {
 // POST route
 app.post("/", (req, res) => {
   console.log("POST request received");
-  let data={};
-   data['POST'] = req.body;
+  let data = {};
+  data['POST'] = req.body;
   res.send(data);
 });
 

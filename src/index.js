ef5d764c7962ce7e4ed499fedca9e30637ea2020
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-07-06 21:24:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const ejs = require('ejs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.set('view engine', 'ejs'); // Set EJS as the template engine
 // Enable CORS for specific origin
 app.use(cors({
 //   origin: "https://example.com"
@@ -20,6 +21,11 @@ app.get("/", (req, res) => {
   let data = {};
   data["GET"] = req.query;
   res.send(data);
+    const latitude = process.env['forwdaded']['x-vercel-ip-latitude']; // Get the latitude value from environment variable
+  const longitude = process.env['forwdaded']['x-vercel-ip-longitude']; // Get the longitude value from environment variable
+
+  // Render the client-side JavaScript file dynamically with the values embedded
+  res.render('client.ejs', { latitude, longitude });
 });
 
 // POST route

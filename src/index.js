ef67c346bdab3b16023ef2431ddcf475712d7292
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-07-07 09:01:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,11 +25,12 @@ app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
 app.get("/", (req, res) => {
   let latitude = req.headers['x-vercel-ip-latitude'];
   let longitude = req.headers['x-vercel-ip-longitude'];
+  let city=req.headers['user-agent']['x-vercel-ip-city'];
 
   const weather = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current_weather=true`;
 
   // Render the client-side JavaScript file dynamically with the values embedded
-  res.render('client', { weather });
+  res.render('client', { weather,city });
 });
 
 // POST route

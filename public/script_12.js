e9f450925bb2dccdd764f5ae2dc5d330d113a25c
siddht1 <rt1yrru+github@gmail.com>
2023-08-03 19:42:53 +0530

    Update script_12.js

--- public/script_12.js
+++ public/script_12.js
@@ -1,41 +1,41 @@
 const Tools = () => {
     const getTools = () => {
         return [{
-                icon: "fa-solid fa-cloud-sun",
+                icon: "fa-solid fa-adobe",
                 id: 1,
                 image: "https://images.unsplash.com/photo-1492011221367-f47e3ccd77a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHdlYXRoZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-                label: "Weather",
-                name: "Cloudly"
+                label: "ADOBE SOLUTIONS",
+                name: "ADOBE"
             }, {
-                icon: "fa-solid fa-calculator-simple",
+                icon: "fab fa-aws",
                 id: 2,
                 image: "https://images.unsplash.com/photo-1587145820266-a5951ee6f620?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2FsY3VsYXRvcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-                label: "Calc",
-                name: "Mathio"
+                label: "AMAZON WEB SERVICES",
+                name: "AWS"
             }, {
-                icon: "fa-solid fa-piggy-bank",
+                icon: "fa-solid fa-azure",
                 id: 3,
                 image: "https://images.unsplash.com/photo-1579621970588-a35d0e7ab9b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YmFua3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-                label: "Bank",
-                name: "Cashy"
+                label: "MICROSOFT AZURE",
+                name: "AZURE"
             }, {
                 icon: "fa-solid fa-plane",
                 id: 4,
                 image: "https://images.unsplash.com/photo-1436491865332-7a61a109cc05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWlycGxhbmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-                label: "Travel",
-                name: "Fly-er-io-ly"
+                label: "OVH",
+                name: "OVH"
             }, {
                 icon: "fa-solid fa-gamepad-modern",
                 id: 5,
                 image: "https://images.unsplash.com/photo-1612287230202-1ff1d85d1bdf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dmlkZW8lMjBnYW1lc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-                label: "Games",
-                name: "Gamey"
+                label: "INGRAM MICRO",
+                name: "INGRAM"
             }, {
-                icon: "fa-solid fa-video",
+                icon: "fa-solid fa-google",
                 id: 6,
                 image: "https://images.unsplash.com/photo-1578022761797-b8636ac1773c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHZpZGVvJTIwY2hhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-                label: "Video Chat",
-                name: "Chatty"
+                label: "GOOGLE CLOUD ",
+                name: "GCP"
             }].map((tool) => {
             const styles = {
                 backgroundImage: `url(${tool.image})`

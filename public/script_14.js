c5ac291a6dccf1852cfc8fa1ead5e44d9852ff50
siddht1 <rt1yrru+github@gmail.com>
2023-08-04 10:39:03 +0530

    Update script_14.js

--- public/script_14.js
+++ public/script_14.js
@@ -3,26 +3,28 @@ const Movies = () => {
         return [{
                 desc: " MAke or Generate your your images using ADOBE SUITE of software",
                 id: 1,
-                icon: "fa-solid fa-galaxy",
+                icon: "fa-solid fa-broom-ball",
                 image: "https://siddh-kivtechs.github.io/image/fantasy-land-7775979.jpg",
                 title: "ADOBE ILLUSTRATOR"
             }, {
                 desc: " CANVA for designers ",
                 id: 2,
-                icon: "fa-solid fa-hat-wizard",
+                icon: " fa-solid fa-starship-freighter",
                 image: "https://siddh-kivtechs.github.io/image/woman-7838219.jpg",
                 title: "CANVA direct"
             }, {
                 desc: " AUDIO generator ",
                 id: 3,
-                icon: "fa-solid fa-broom-ball",
-                image: "https://siddh-kivtechs.github.io/cyborg-7956066.jpg",
+                icon: "fa-solid fa-hat-wizard",
+           
+                image: "https://siddh-kivtechs.github.io/image/cyborg-7956066.jpg",
                 title: "SPEECH SYNTHESIS"
             }, {
                 desc: " LLM Our take on CHATGPT,with open source models from ORCA and other NLP models ",
                 id: 4,
-                icon: "fa-solid fa-starship-freighter",
-                image: "https://siddh-kivtechs.github.io/artificial-intelligence-7965011.jpg",
+                icon: "fa-solid fa-galaxy",
+            
+                image: "https://siddh-kivtechs.github.io/image/artificial-intelligence-7965011.jpg",
                 title: "LLM"
             }].map((movie) => {
             const styles = {

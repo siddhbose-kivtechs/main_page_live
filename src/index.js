d0478f70a6f9c01b6a80cfd496ccb4a09f29a5d8
siddht1 <rt1yrru+github@gmail.com>
2023-09-23 23:58:27 +0530

    Update index.js
    re-added mistakenly deleted functioning code

--- src/index.js
+++ src/index.js
@@ -26,21 +26,62 @@ app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
 app.get("/", baseHandler);
 
 async function baseHandler(req, res) {
-  // ... existing code ...
+  const log = {
+    lat: req.headers['x-vercel-ip-latitude'],
+    lon: req.headers['x-vercel-ip-longitude'],
+    location: req.headers['x-vercel-ip-city'] + ',' + req.headers['x-vercel-ip-country-region'] + ',' + req.headers['x-vercel-ip-country'],
+    IP: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+    UA: req.headers['user-agent'],
+    uuid: uuidv4(),
+    date_time: new Date()
+    // date_time: new Date().toLocaleString(undefined, { timeZone: 'user' }) // Change the date time to user's time
+  };
 
   const { data, error } = await supabase.from('visitor').insert([log]);
 
-  // ... existing code ...
+  if (error) {
+    console.error('Error inserting log:', error);
+    res.status(500).send('Error inserting log');
+  } else {
+    console.log('Log inserted successfully:', data);
+  }
+
+  let ip_address = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  redirectView(req.query.view, res, req.headers, ip_address);
 }
 
 function redirectView(view, res, headers, ip_address) {
-  // ... existing code ...
+  let latitude = headers['x-vercel-ip-latitude'];
+  let longitude = headers['x-vercel-ip-longitude'];
+  let location = headers['x-vercel-ip-city'] + ',' + headers['x-vercel-ip-country-region'] + ',' + headers['x-vercel-ip-country'];
+
+  switch (view) {
+    case 'chatbot':
+      res.redirect("https://siddh-kivtechs.github.io/menu_4/");
+      break;
+    case 'ovh':
+      res.redirect("https://ovh.kivtechs.cloud/");
+      break;
+    case 'adobe':
+      res.redirect("https://siddh-kivtechs.github.io/kivtechs/");
+      break;
+    case 'tts':
+      res.redirect("https://jstts1.kivtechs.cloud/");
+      break;
+    case 'image_api':
+      res.redirect("https://image.kivtechs.cloud/");
+      break;
+    case 'login':
+      res.redirect('https://siddh-kivtechs.github.io/login_sample/');
+      break;
+    default:
+      res.render('client', { latitude, longitude, location, ip_address });
+  }
 }
 
 app.get('/chao', (req, res) => {
-  // ... existing code ...
+  res.redirect("https://siddh-kivtechs.github.io/menu_4/");
 });
-
 app.post("/", async (req, res) => {
   try {
     console.log("POST request received");

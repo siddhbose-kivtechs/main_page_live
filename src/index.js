7aaccef50e47a753e8cde06527a871247bf5a65a
siddht1 <rt1yrru+github@gmail.com>
2023-09-23 14:09:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const ejs = require('ejs');
 const path = require("path");
-const { sql } = require("@vercel/postgres");
+const { get, set } = require('@vercel/edge-config');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs'); // Set EJS as the template engine
 app.set('view engine', 'ejs'); // Set EJS as the template engine
 app.set('views', path.join(__dirname, '../views')); // Update the path to '/views'
 
@@ -18,30 +17,31 @@ app.use(express.static(path.join(__dirname, '../public'))); // Update the path t
 // Enable CORS for specific origin
 app.use(cors());
 
-// test postgress
-//const likes = 100;
-
-//const { rows } = await sql`SELECT * FROM posts WHERE likes > ${likes};`;
-
 // Parse request body and extend the size to 1mb
 app.use(bodyParser.json({ limit: '1mb' }));
 app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
 
-
 app.get("/", baseHandler);
 
-function baseHandler(req, res) {
-  let latitude = req.headers['x-vercel-ip-latitude'];
-  let longitude = req.headers['x-vercel-ip-longitude'];
-  let location = req.headers['x-vercel-ip-city'] + ',' + req.headers['x-vercel-ip-country-region'] + ',' + req.headers['x-vercel-ip-country'];
+async function baseHandler(req, res) {
+  let latitude = get(req.headers, 'x-vercel-ip-latitude');
+  let longitude = get(req.headers, 'x-vercel-ip-longitude');
+  let location = `${get(req.headers, 'x-vercel-ip-city')},${get(req.headers, 'x-vercel-ip-country-region')},${get(req.headers, 'x-vercel-ip-country')}`;
 
   const weather = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current_weather=true`;
-let ip_address = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
- redirectView(req.query.view, res, latitude,longitude, location,ip_address);
+  let ip_address = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
+  // Store IP, user-agent, latitude, longitude, and place in the Edge Config database
+  await set('ip', ip_address);
+  await set('user-agent', req.headers['user-agent']);
+  await set('latitude', latitude);
+  await set('longitude', longitude);
+  await set('place', location);
+
+  redirectView(req.query.view, res, latitude, longitude, location, ip_address);
 }
 
-function redirectView(view, res, latitude,longitude, location,ip_address) {
+function redirectView(view, res, latitude, longitude, location, ip_address) {
   switch (view) {
     case 'chatbot':
       res.redirect("https://siddh-kivtechs.github.io/menu_4/");
@@ -62,12 +62,13 @@ function redirectView(view, res, latitude,longitude, location,ip_address) {
       res.direct('https://siddh-kivtechs.github.io/login_sample/');
       break;
     default:
-      res.render('client', { latitude,longitude, location,ip_address });
+      res.render('client', { latitude, longitude, location, ip_address });
   }
 }
+
 app.get('/chao', (req, res) => {
   // Handle GET request to /chao path
-     res.redirect("https://siddh-kivtechs.github.io/menu_4/");
+  res.redirect("https://siddh-kivtechs.github.io/menu_4/");
 });
 
 // POST route

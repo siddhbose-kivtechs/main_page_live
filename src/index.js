a7051fa5351038c47c96c9265c72b150f3cf2268
siddht1 <rt1yrru+github@gmail.com>
2023-11-19 21:53:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,12 +7,11 @@ const { createClient } = require('@supabase/supabase-js');
 
 require('dotenv').config();
 const ejs = require('ejs');
-const path = require("path");
-const cors = require("cors");
+const path = require('path');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
-const COMPANYNAME='Kivtechs';
-
+const COMPANYNAME = 'Kivtechs';
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
@@ -24,11 +23,11 @@ const config = {
   secret: process.env.AUTHO_secret,
   baseURL: process.env.AUTHO_baseURL,
   clientID: process.env.AUTHO_CLIENT_ID,
-  issuerBaseURL: process.env.AUTHO_BASEURI_issuer
+  issuerBaseURL: process.env.AUTHO_BASEURI_issuer,
 };
 
 const app = express();
-app.use(morgan('combined'));
+app.use(morgan('combined', { stream: fs.createWriteStream('access.log') })); // Log requests to a file instead of the console
 app.use(helmet());
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
@@ -36,21 +35,27 @@ app.use(auth(config));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
-const dashEjsPath = path.join(__dirname, "../views/dash.ejs");
+const dashEjsPath = path.join(__dirname, '../views/dash.ejs');
 
 app.use(express.static(path.join(__dirname, '../public')));
-app.use(cors());
-// app.use(bodyParser.json({ limit: '1mb' }));
-// app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
+app.use(cors({ origin: ['http://localhost:3000', 'https://your-domain.com'] })); // Allow specific origins for CORS
+app.use(express.json({ limit: '1mb' })); // Parse JSON data
+app.use(express.urlencoded({ limit: '1mb', extended: true })); // Parse form data
 
+// Protected route that requires authentication
+app.get('/profile', requiresAuth(), (req, res) => {
+  console.log(req.oidc);
+  res.send(JSON.stringify(req.oidc.user));
+});
 
-// // req.isAuthenticated is provided from the auth router
-// app.get('/', (req, res) => {
-//     const user = req.oidc.user;
-//     console.log(user)
-//   // res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-//      res.render(dashEjsPath, { user });
-// });
+// Protected route that requires authentication
+app.get('/protected', requiresAuth(), (req, res) => {
+  console.log(req.oidc);
+  res.send('Access to protected resource granted');
+  console.log(req.oidc);
+});
+
+// Not protected route
 app.get('/', (req, res) => {
   if (req.oidc.isAuthenticated()) {
     const user = req.oidc.user;
@@ -61,26 +66,7 @@ app.get('/', (req, res) => {
   }
 });
 
-app.get('/profile', requiresAuth(), (req, res) => {
-  console.log(req.oidc);
-  res.send(JSON.stringify(req.oidc.user));
-});
-
-// Protected route that requires authentication
-app.get('/protected', (req, res) => {
-   console.log(req.oidc);
-  if (req.oidc.isAuthenticated()) {
-    // User is authenticated, access protected resource
-    res.send('Access to protected resource granted');
-     console.log(req.oidc);
-  } else {
-    // User is not authenticated, redirect to login
-    res.redirect('/login');
-     console.log(req.oidc);
-  }
-});
-
 // Start the server
 app.listen(PORT, () => {
-  console.log(COMPANYNAME+'  listening on port :'+PORT);
+  console.log(COMPANYNAME + '  listening on port :' + PORT);
 });

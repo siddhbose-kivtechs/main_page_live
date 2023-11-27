cf44a88a5cf654111262e6223edd64d99e25e2d0
siddht1 <rt1yrru+github@gmail.com>
2023-11-27 08:24:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -133,7 +133,7 @@ app.get('/', (req, res) => {
   if (req.oidc.isAuthenticated()) {
     const user = req.oidc.user;
     console.log(user);
-    res.render(userEjsPath, { user } }); // Render the dashboard for logged-in users
+    res.render(userEjsPath, { user } ); // Render the dashboard for logged-in users
   } else {
     res.redirect('/login'); // Redirect non-logged-in users to the login page
   }

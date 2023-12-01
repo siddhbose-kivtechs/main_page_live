300d7ed962d20675926c703de3d3a42973ad0b91
siddht1 <rt1yrru+github@gmail.com>
2023-12-02 00:14:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -147,20 +147,6 @@ app.all([ '/login'], (req, res) => {
 });
 
 
-app.all([ '/login/callback'], (req, res) => {
-
-  if (req.oidc.isAuthenticated()) {
-    user = req.oidc.user;
-      res.render(userEjsPath, { user });
-    
-  } 
-  else {
-   console.log(' Missing config');
-    res.redirect('/login');
-  }
-
-
-});
 
 
 //  if none then send 404
